5ceffbdc9dbf9e34005adc8bd36ee4c26de74008
Jordan Masone <jordanmasone1@gmail.com>
2021-07-10 19:57:27 -0400

    Added status and activity to ready event.

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = {
     name: "ready",
     once: true,
     execute(client) {
+        client.user.setPresence({ activity: { name: "your commands!", type: "LISTENING" }, status: "online" })
         console.log(`Ready! My name is ${client.user.username} and I am here to help :)`);
     },
 };
