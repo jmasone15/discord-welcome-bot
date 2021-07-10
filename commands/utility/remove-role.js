dc3658eb875029b3d9a497bddc7c8e26e14d8ffd
Jordan Masone <jordanmasone1@gmail.com>
2021-07-10 19:00:05 -0400

    Added remove role command.

--- commands/utility/remove-role.js
+++ commands/utility/remove-role.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: "add-role",
-    description: "Give a role to a targeted user.",
+    name: "remove-role",
+    description: "Remove a role or all roles from a targeted user.",
     guildOnly: true,
     cooldown: 1,
     args: true,
@@ -12,13 +12,13 @@ module.exports = {
 
         // Validation
         if (!taggedUser) {
-            return message.reply("Specify who you want to give a role by tagging them.");
-        }
-        if (args.length < 2) {
-            return message.reply("Specify which role you want to give the user.");
+            return message.reply("Specify who you want to remove a role from by tagging them.");
         }
         if (args.length > 2) {
             return message.reply("Only one role at a time.");
+        }
+        if (args.length === 1) {
+            return message.reply(`Please enter the role you want to remove from ${taggedUser} after the user's name.`)
         }
 
         const role = message.guild.roles.cache.find(role => role.name === args[1]);
@@ -26,18 +26,18 @@ module.exports = {
 
         if (taggedMember) {
             if (taggedMember.roles.cache.some(r => r.name === role.name)) {
-                return message.reply(`${taggedUser} already has the role ${role.name}!`);
-            } else {
                 try {
-                    await taggedMember.roles.add(role);
-                    message.channel.send(`${taggedUser} has successfully been given the role ${role.name}.`);
+                    await taggedMember.roles.remove(role);
+                    message.channel.send(`${role.name} has been successfully removed from ${taggedUser}.`);
                 } catch (err) {
                     console.error(err);
                     message.reply("There was an error with this command!");
                 }
+            } else {
+                return message.reply(`${taggedUser} does not have the role ${role.name}!`);
             }
         } else {
-            message.reply("You cannot give a role to someone who isn't in the guild.")
+            message.reply("You cannot remove a role from someone who isn't in the guild.")
         }
     }
 }
