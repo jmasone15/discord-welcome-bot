4aecde92b3762faf1c601752a217d7d8e97a9e57
Jordan Masone <jordanmasone1@gmail.com>
2021-07-11 01:25:08 -0400

    Added an embed about me command.

---
+++ commands/fun/about.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "about",
+    description: "Command to learn about bot creator.",
+    cooldown: 5,
+    async execute(message, args) {
+        const aboutEmbed = new Discord.MessageEmbed()
+            .setColor("#7700ff")
+            .setTitle("Hi, I'm Jordan!")
+            .setURL("https://github.com/jmasone15")
+            .setDescription("I created this bot!")
+            .setThumbnail("https://cdn.discordapp.com/avatars/420651836157067275/a_01603ec5cd3a13e039c34c2785d630b4.gif")
+            .addField("About Me", "I am a Full Stack Developer based out of Orlando.")
+            .setTimestamp()
+
+        try {
+            await message.channel.send(aboutEmbed);
+        } catch (err) {
+            console.error(err);
+            message.reply("Error using this command.");
+        }
+    }
+}
