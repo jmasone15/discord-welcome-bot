9c6f6539068d67c185860a6e655abf831c6499ae
Jordan Masone <jordanmasone1@gmail.com>
2021-07-14 14:39:10 -0400

    Heroku error #1

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
-const { prefix } = require("../../config.json");
+// const { prefix } = require("../../config.json");
+const prefix = "!";
 
 module.exports = {
     name: "help",
